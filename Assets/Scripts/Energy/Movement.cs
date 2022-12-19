a0aa8cec224eaf07c09142d7fafd86da2fcffc0a
positive-season <scp94373@gmail.com>
2022-12-19 20:44:08 +0000

    Energy moves towards Chakra, can destroy towers

--- Assets/Scripts/Energy/Movement.cs
+++ Assets/Scripts/Energy/Movement.cs
@@ -8,39 +8,19 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private GameObject chakra;
     private int trackPointIndex = 0;
-    [SerializeField] private SOEnergy soEnergy;
-    [SerializeField] private GameObject allTracks; //Rename to tracks
-    [SerializeField] private GameObject selectedTrack;
-    [SerializeField] private GameObject leftTrack; 
-    [SerializeField] private GameObject midTrack; 
-    [SerializeField] private GameObject rightTrack; 
-    [FormerlySerializedAs("pathway")] [SerializeField] private GameObject track;
-
-    [FormerlySerializedAs("pathways")] [SerializeField] private GameObject[] tracks;
-
-    [SerializeField] private int count = 0;
-
+    private SOEnergy soEnergy;
     [FormerlySerializedAs("speed")] [SerializeField] private float baseSpeed = 0.1f;
-    private float defaultSpeed;
 
-    [SerializeField] private Vector3 startingPos;
-    
     // Start is called before the first frame update
     void Start()
     {
-        SetStartingPosition(transform.position);
-        SetSpeed();
-        SubEvents();
+        
     }
     
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, tracks[count].transform.position) < 1)
-        {
-            if(count <= tracks.Length) count++;
-        }
-        transform.position = Vector3.MoveTowards(transform.position, tracks[count].transform.position, baseSpeed);
+        transform.position = Vector3.MoveTowards(transform.position, chakra.transform.position, baseSpeed);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -49,92 +29,25 @@ public class Movement : MonoBehaviour
         {
             Debug.Log("Energy absorbed");
             ReturnToPool();
-            count = 0;
+        } else if (other.gameObject.CompareTag("Portal"))
+        {
+            Debug.Log("Energy absorbed");
+            ReturnToPool();
+        } else if (other.gameObject.CompareTag("Tower"))
+        {
+            Debug.Log("Energy Destroyed");
+            other.gameObject.GetComponent<TowerHealth>().TakeDamage();
+            ReturnToPool();
         }
     }
     
-    private void SetupPathway(GameObject _selectedTrack)
-    {
-        foreach (Transform trackPoint in _selectedTrack.transform)
-        {
-            tracks[trackPointIndex] = trackPoint.gameObject;
-            Debug.Log(trackPoint);
-            trackPointIndex++;
-        }
-        
-        if (trackPointIndex == _selectedTrack.transform.childCount)
-        {
-            tracks[trackPointIndex] = chakra.gameObject;
-        }
-        
-    }
-
-    private void SubEvents()
-    {
-        GameObject.Find("GameManager").GetComponent<Speed>().GameSpeedUpdated += SetGameSpeed;
-    }
-
-    private void SetGameSpeed(int _multiplier)
-    {
-        if (_multiplier == 2)
-        {
-            baseSpeed *= _multiplier;
-        }
-        else
-        {
-            baseSpeed = defaultSpeed;
-        }
-    }
-
-    private void UnsubEvents()
-    {
-        GameObject.Find("GameManager").GetComponent<Speed>().GameSpeedUpdated -= SetGameSpeed;
-    }
-
     private void ReturnToPool()
     {
         Debug.Log("Returned to Pool");
         Destroy(gameObject);
         //TODO Return to pool
-        UnsubEvents();
-    }
-
-    public void SetTrack(int _trackIndex)
-    {
-        GameObject _track = allTracks.transform.GetChild(_trackIndex).gameObject;
-        selectedTrack = _track;
-        Debug.Log(selectedTrack);
-        SetupPathway(selectedTrack);
-        switch (_trackIndex)
-        {
-            case 0:
-                SetStartingPosition(new Vector3(transform.position.x - 3, transform.position.y, transform.position.z));
-                transform.position = startingPos;
-                break;
-            case 1:
-                SetStartingPosition(transform.position);
-                transform.position = startingPos;
-                break;
-            case 2:
-                SetStartingPosition(new Vector3(transform.position.x + 3, transform.position.y, transform.position.z));
-                transform.position = startingPos;
-                break;
-            default:
-                break;
-        }
-    }
-
-    public void SetSpeed()
-    {
-        baseSpeed = baseSpeed *= GameObject.Find("GameManager").GetComponent<Speed>().GetGameSpeed() * soEnergy.speed;
-        defaultSpeed = baseSpeed;
     }
     
-    private void SetStartingPosition(Vector3 startPos)
-    {
-        startingPos = startPos;
-        transform.position = startPos;
-    }
 
     public void SetSOEnergy(SOEnergy _soEnergy)
     {
--- Assets/Scripts/GameManager/SpawnEnergy.cs
+++ Assets/Scripts/GameManager/SpawnEnergy.cs
@@ -6,51 +6,45 @@ using UnityEngine.Serialization;
 public class SpawnEnergy : MonoBehaviour
 {
     [SerializeField] private GameObject energy; 
-    [SerializeField] private GameObject startPosition;
-    private ObjectCount _objectCount;
     [FormerlySerializedAs("soEnergy")] [SerializeField] private SOEnergy soEnergyBasic;
-    [FormerlySerializedAs("soEnergy")] [SerializeField] private SOEnergy soEnergyFast;
-    [FormerlySerializedAs("soEnergy")] [SerializeField] private SOEnergy soEnergyFierce;
-    [FormerlySerializedAs("soEnergy")] [SerializeField] private SOEnergy soEnergyDefencive;
-    [SerializeField] private MoneyManagement moneyManagement;
-   
+    [SerializeField] private GameObject startPosition;
+    [SerializeField] private float spawnTime = 1f;
+
     private void Start()
     {
-        _objectCount = GameObject.Find("GameManager").GetComponent<ObjectCount>();
-    }
-    public void SpawnBasic(int trackPoint)
-    {
-        SpawnFromPool(trackPoint, soEnergyBasic);
+        InvokeRepeating (nameof(SpawnBasic), 1f, 1f);
+        SpawnBasic();
     }
 
-    private void SpawnFromPool(int trackPoint, SOEnergy energyObject)
+    private void Update()
     {
-        switch (energyObject.type)
+        
+    }
+
+    public void SpawnBasic()
+    {
+        SpawnFromPool(soEnergyBasic);
+    }
+
+    private void SpawnFromPool(SOEnergy soEnergyBasic)
+    {
+        switch (soEnergyBasic.type)
         {
             case "Basic":
             case "basic":
-                SpawnEnergyofType(trackPoint, soEnergyBasic);
+                SpawnEnergyofType(soEnergyBasic);
                 break;
             default:
                 break;
         }
         
     }
-
-    private void SpawnEnergyofType(int trackPoint, SOEnergy energyObject)
+    
+    private void SpawnEnergyofType(SOEnergy energyObject)
     {
-        if (moneyManagement.DecreaseMoney(energyObject.price)) //10 being the cost of the basic Energy
-        {
-            float gameSpeedMultiplier = GameObject.Find("GameManager").GetComponent<Speed>().GetGameSpeed();
-            GameObject _energy = Instantiate(energy, startPosition.transform.position, startPosition.transform.rotation);
-            _energy.GetComponent<Movement>().SetSOEnergy(energyObject);
-            _energy.GetComponent<Movement>().SetTrack(trackPoint);
-            _energy.GetComponent<Health>().SetHealth(energyObject.health);
-            _objectCount.Add(_energy);
-        }
-        else
-        {
-            Debug.Log("Unable to spawn Energy, not enough money");
-        }
+        GameObject _energy = Instantiate(energy, startPosition.transform.position, startPosition.transform.rotation);
+        _energy.GetComponent<Health>().SetHealth(energyObject.health);
+
     }
+
 }
