970bc32a26fc705beba9c3719ce00abae97dcc9f
positive-season <scp94373@gmail.com>
2022-12-28 10:46:37 +0000

    Unlock Lanes button now sets the other lanes to active

--- Assets/Scripts/GameManager/Upgrades/UnlockLane.cs
+++ Assets/Scripts/GameManager/Upgrades/UnlockLane.cs
@@ -8,6 +8,7 @@ public class UnlockLane : MonoBehaviour, IUpgrade
     [SerializeField] private GameObject unlockLane;
     [SerializeField] private int level = 1;
     [SerializeField] private float price = 1f;
+    [SerializeField] private GameObject lanes;
     private Purchasing purchasing;
     // Start is called before the first frame update
     void Start()
@@ -29,6 +30,7 @@ public class UnlockLane : MonoBehaviour, IUpgrade
         price += (price * 0.1f);
         unlockLane.transform.Find("Level").GetComponent<TMP_Text>().text = level.ToString();
         unlockLane.transform.Find("Price").GetComponent<TMP_Text>().text = price.ToString();
+        bool laneUnlocked = UnlockNextLane();
     }
 
     public int GetLevel()
@@ -41,5 +43,28 @@ public class UnlockLane : MonoBehaviour, IUpgrade
         return price;
     }
 
-    
+    private bool UnlockNextLane()
+    {
+        List<GameObject> lockedLanes = new List<GameObject>();
+        for (int i = 0; i < lanes.transform.childCount; i++)
+        {
+            GameObject child = lanes.transform.GetChild(i).gameObject;
+            if (!child.activeInHierarchy)
+            {
+                lockedLanes.Add(lanes.transform.GetChild(i).gameObject);
+            }
+        }
+
+        if (lockedLanes.Count > 0)
+        {
+            lockedLanes[0].SetActive(true);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
 }
