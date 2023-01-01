2b860416f19b2289f6f4b8b90ece181d50212f10
positive-season <scp94373@gmail.com>
2023-01-01 06:14:17 +0000

    Buttons now disable and enable based upon money vs price

--- Assets/Scripts/GameManager/Upgrades/EnergySpawnRate.cs
+++ Assets/Scripts/GameManager/Upgrades/EnergySpawnRate.cs
@@ -4,20 +4,24 @@ using System.Collections.Generic;
 using log4net.Core;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnergySpawnRate : MonoBehaviour, IUpgrade
 {
     [SerializeField] private GameObject upgradeSpawnRate;
+    [SerializeField] private GameObject upgradeSpawnRateButton;
     [SerializeField] private int level = 1;
     [SerializeField] private float price = 1f;
     [SerializeField] private SpawnEnergy _spawnEnergy;
     private Purchasing purchasing;
+    private CurrencyManager currencyManager;
 
     public event Action onUpgrade;
     // Start is called before the first frame update
     void Start()
     {
         purchasing = GetComponent<Purchasing>();
+        currencyManager = GetComponent<CurrencyManager>();
         SubToEvents();
         upgradeSpawnRate.transform.Find("Level").GetComponent<TMP_Text>().text = level.ToString();
         upgradeSpawnRate.transform.Find("Price").GetComponent<TMP_Text>().text = price.ToString();
@@ -26,6 +30,7 @@ public class EnergySpawnRate : MonoBehaviour, IUpgrade
     private void SubToEvents()
     {
         purchasing.onEnergySpawnRateUpgrade += OnPurchase;
+        currencyManager.onMoneyUpdate += OnMoneyUpdate;
     }
 
     public void OnPurchase()
@@ -36,6 +41,20 @@ public class EnergySpawnRate : MonoBehaviour, IUpgrade
         upgradeSpawnRate.transform.Find("Price").GetComponent<TMP_Text>().text = price.ToString();
         onUpgrade?.Invoke();
         // _spawnEnergy.DecreaseSpawnTime();
+    }
+    
+    private void OnMoneyUpdate(double money)
+    {
+        if (currencyManager.CanPurchase(price))
+        {
+            //TODO: Enable Unlock Lane button (if disabled)
+            upgradeSpawnRateButton.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            //TODO: Disable button (if enabled)
+            upgradeSpawnRateButton.GetComponent<Button>().interactable = false;
+        }
     }
     
     private void ResetValues()
--- Assets/Scripts/GameManager/Upgrades/EnergyValue.cs
+++ Assets/Scripts/GameManager/Upgrades/EnergyValue.cs
@@ -3,14 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnergyValue : MonoBehaviour, IUpgrade
 {
     [SerializeField] private GameObject upgradeEnergyValue;
+    [SerializeField] private GameObject upgradeEnergyValueButton;
     [SerializeField] private int level = 1;
     [SerializeField] private float price = 1f;
 
     private Purchasing purchasing;
+    private CurrencyManager currencyManager;
     
     public event Action onUpgrade;
     
@@ -18,6 +21,7 @@ public class EnergyValue : MonoBehaviour, IUpgrade
     void Start()
     {
         purchasing = GetComponent<Purchasing>();
+        currencyManager = GetComponent<CurrencyManager>();
         SubToEvents();
         upgradeEnergyValue.transform.Find("Level").GetComponent<TMP_Text>().text = level.ToString();
         upgradeEnergyValue.transform.Find("Price").GetComponent<TMP_Text>().text = price.ToString();
@@ -27,6 +31,7 @@ public class EnergyValue : MonoBehaviour, IUpgrade
     private void SubToEvents()
     {
         purchasing.onEnergyValueUpgrade += OnPurchase;
+        currencyManager.onMoneyUpdate += OnMoneyUpdate;
     }
 
     private void OnPurchase()
@@ -45,6 +50,20 @@ public class EnergyValue : MonoBehaviour, IUpgrade
         price = 1;
         upgradeEnergyValue.transform.Find("Level").GetComponent<TMP_Text>().text = level.ToString();
         upgradeEnergyValue.transform.Find("Price").GetComponent<TMP_Text>().text = price.ToString();
+    }
+    
+    private void OnMoneyUpdate(double money)
+    {
+        if (currencyManager.CanPurchase(price))
+        {
+            //TODO: Enable Unlock Lane button (if disabled)
+            upgradeEnergyValueButton.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            //TODO: Disable button (if enabled)
+            upgradeEnergyValueButton.GetComponent<Button>().interactable = false;
+        }
     }
 
 
